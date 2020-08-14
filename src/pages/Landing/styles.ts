1371c64283a515ad4ba11dfc1784d76e42f8db94
Matheus Castro <matheus.castroks@gmail.com>
2020-08-14 03:19:40 -0300

    finalizada as paginas

---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+import { Poppins_400Regular, Poppins_600SemiBold, Poppins_400Regular_Italic } from '@expo-google-fonts/poppins';
+import { Archivo_400Regular } from '@expo-google-fonts/archivo';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8257e5',
+    justifyContent: 'center',
+    padding: 40
+
+  },
+
+  banner: {
+    width: '100%',
+    resizeMode: 'contain'
+  },
+
+  title: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#FFF',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 20,
+  },
+
+  titleBold: {
+    fontFamily: 'Poppins_600SemiBold'
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between',
+
+  },
+
+  button: {
+    height: 150,
+    width: '48%',
+    backgroundColor: '#333',
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: "space-between",
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871f5'
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04d361',
+  },
+
+  buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 20,
+  },
+
+  totalConnections: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#c4d2ff',
+    fontSize: 14.5,
+    lineHeight: 20,
+    maxWidth: 350,
+    marginTop: 40,
+  }
+
+});
+
+export default styles;
